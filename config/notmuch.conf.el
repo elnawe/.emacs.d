6058c6473ea8d274cf8acfb5622a68b90498a4f8
Nahuel Jesús Sacchetti <me@nsacchetti.com>
2019-08-31 13:15:17 -0500

    Add rjsx-mode and prog-mode config. Modify notmuch.

--- config/notmuch.conf.el
+++ config/notmuch.conf.el
@@ -1,1 +1,10 @@
-(use-package notmuch)
+(use-package notmuch
+  :bind
+  (("C-x m" . notmuch))
+  :custom
+  (notmuch-search-oldest-first nil)
+  (notmuch-search-result-format `(("date" . "%12s ")
+                                  ("count" . "%-5s ")
+                                  ("authors" . "%-20s ")
+                                  ("subject" . "%s ")
+                                  ("tags" . "(%s)"))))
---
+++ config/prog-mode.conf.el
@@ -0,0 +1,9 @@
+(use-package rjsx-mode
+  :mode "\\.jsx\\'"
+  :custom
+  (js-indent-level 4))
+
+(use-package sgml-mode
+  :ensure nil
+  :custom
+  (sgml-basic-offset 4))
