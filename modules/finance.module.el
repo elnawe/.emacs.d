5049869ba090f57193d7ffbe16f96d1fc0266873
Nahuel Jesús Sacchetti <me@nsacchetti.com>
2020-10-15 20:08:26 -0500

    Add setup code

--- modules/finance.module.el
+++ modules/finance.module.el
@@ -1,15 +1,16 @@
-(require 'ledger-mode)
+(when nemacs-module-finance-enabled
+  (require 'ledger-mode)
 
-(with-eval-after-load 'ledger-mode
-  (require 'nemacs-org-ledger)
+  (with-eval-after-load 'ledger-mode
+    (require 'nemacs-org-ledger)
 
-  (defun nemacs-setup-ledger-mode ()
-    "NEMACS Setup: Run this function in `ledger-mode-hook'."
-    (company-mode)
-    (setq-local complete-cycle-threshold t)
-    (setq-local ledger-complete-in-steps t)
-    (setq-local tab-always-indent 'complete))
+    (defun nemacs-setup-ledger-mode ()
+      "NEMACS Setup: Run this function in `ledger-mode-hook'."
+      (company-mode)
+      (setq-local complete-cycle-threshold t)
+      (setq-local ledger-complete-in-steps t)
+      (setq-local tab-always-indent 'complete))
 
-  (add-hook 'ledger-mode-hook #'nemacs-setup-ledger-mode)
+    (add-hook 'ledger-mode-hook #'nemacs-setup-ledger-mode)
 
-  (setq ledger-master-file (nemacs-ledger-file "booking.ledger")))
+    (setq ledger-master-file (nemacs-ledger-file "booking.ledger"))))
