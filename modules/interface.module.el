d8cdc81f662967f528182ded172a77df7938f7f0
Nahuel Jesús Sacchetti <me@nsacchetti.com>
2020-12-26 11:53:29 -0600

    Change legacy code (to be removed) to new mode-line

--- modules/interface.module.el
+++ modules/interface.module.el
@@ -25,16 +25,20 @@
 (diminish 'subword-mode)
 
 (setq-default mode-line-format
-      '("%e"
-        mode-line-front-space
-        mode-line-modified
-        " "
-        mode-line-buffer-identification
-        " "
-        mode-line-position
-        " "
-        mode-line-modes
-        mode-line-misc-info))
+              '("%e"
+                mode-line-front-space
+                mode-line-mule-info
+                mode-line-client
+                mode-line-modified
+                mode-line-remote
+                mode-line-frame-identification
+                mode-line-buffer-identification
+                "   "
+                mode-line-position
+                (vc-mode vc-mode)
+                "  "
+                mode-line-misc-info
+                mode-line-end-spaces))
 
 (with-eval-after-load 'all-the-icons
   (setq all-the-icons-mode-icon-alist
